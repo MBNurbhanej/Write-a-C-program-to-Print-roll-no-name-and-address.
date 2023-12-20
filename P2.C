1abdc66716856f3a22e746ff4ee18d384c63ba0b
MohammadAli B. Nurbhanej <153924654+MBNurbhanej@users.noreply.github.com>
2023-12-20 07:58:15 +0530

    Add files via upload

---
+++ P2.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	clrscr();
+	printf("Roll No=1\n");
+	printf("Name=Mammu\n");
+	printf("Address=Vidhyanagari BCA College,Himmatnagar");
+	getch();
+}
